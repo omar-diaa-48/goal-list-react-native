758dd657e8f01b0bd749bf1d4521a9ae1a92ffa3
omar <o.wakeel@intouch.com>
2023-12-20 16:56:59 +0200

    feat: adjust goal item component

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 
 const GoalItem = ({ item, handleDelete }) => {
     return (
         <View style={styles.goalItem}>
             <Text>#{item.index + 1} {item.item}</Text>
-            <Text style={styles.goalDeleteItem} onPress={() => handleDelete(item.index)}>x</Text>
+            <Pressable onPress={() => handleDelete(item.index)}>
+                <Text style={styles.goalDeleteItem}>x</Text>
+            </Pressable>
         </View>
     )
 }
